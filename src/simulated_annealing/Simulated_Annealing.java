dbce171568feb19da5753ae8fbcf816c5728fb59
bastomy <bastomy67@gmail.com>
2018-09-16 18:02:39 +0700

    still error fix later

--- src/simulated_annealing/Simulated_Annealing.java
+++ src/simulated_annealing/Simulated_Annealing.java
@@ -1,16 +1,13 @@
 package simulated_annealing;
 
 /**
- * @author Bastomy 
- * ID       : 1301178418 
- * Class    : IF-40-GAB05 
+ * @author Bastomy ID : 1301178418 Class : IF-40-GAB05
  */
-
 public class Simulated_Annealing {
 
     public static void main(String[] args) {
         Simulated_Annealing run = new Simulated_Annealing();
-        System.out.println(run.theFunction(0.1, 0.1));
+        run.mainFunction();
     }
 
     private double theFunction(double x1, double x2) {
@@ -19,9 +16,41 @@ public class Simulated_Annealing {
         sin = Math.sin(x1);
         cos = Math.cos(x2);
         sqrt = Math.sqrt(Math.pow(x1, 2) + Math.pow(x2, 2)) / Math.PI;
-        value = - Math.abs(sin * cos * Math.exp(Math.abs(1 - sqrt)));
+        value = -Math.abs(sin * cos * Math.exp(Math.abs(1 - sqrt)));
 
         return value;
+    }
+
+    private void mainFunction() {
+        double e0, eCurrent, eNew, deltaE, p, random;
+        double bfs1 = 0, bfs2 = 0;   //Best So Far
+        int temp = 2000;    //Temperature 
+        double x1 = -10;    // initial value
+        double x2 = -10;    // initial value
+
+        e0 = theFunction(x1, x2);
+        eCurrent = e0;
+        while (temp != 0) {
+            x1 = x1 + 0.01;
+            x2 = x2 + 0.01;
+            eNew = theFunction(x1, x2);
+            deltaE = eNew - eCurrent;
+            if (deltaE < 0) {
+                eCurrent = eNew;
+                bfs1 = x1;
+                bfs2 = x2;
+            } else {
+                p = Math.pow(Math.E, -deltaE / temp);
+                random = Math.random();
+                if (random < p) {
+                    eCurrent = eNew;
+                }
+            }
+            temp--;
+        }
+        System.out.println("X1 " + bfs1);
+        System.out.println("X2 " + bfs2);
+        System.out.println("Hasil " + eCurrent);
     }
 
 }
